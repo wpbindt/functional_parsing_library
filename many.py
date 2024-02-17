11323b7240a1b2c2e2624ba4423200e3feb76760
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-02-17 02:09:20 +0100

    Add test for many

---
+++ many.py
@@ -0,0 +1,26 @@
+import unittest
+
+from asserts import assert_parsing_fails, assert_parsing_succeeds
+from char import char
+from parser import Parser, T
+
+
+def many(parser: Parser[T]) -> Parser[list[T]]:
+    return (lambda x: [x]) * parser
+
+
+class TestMany(unittest.TestCase):
+    def test_many_fails_to_parse_unparsable(self) -> None:
+        parser = many(char('a'))
+
+        assert_parsing_fails(self, parser, 'b')
+
+    def test_many_parses_one(self) -> None:
+        parser = many(char('a'))
+
+        assert_parsing_succeeds(self, parser, 'a').with_result(['a'])
+
+    def test_many_parses_two(self) -> None:
+        parser = many(char('a'))
+
+        assert_parsing_succeeds(self, parser, 'aa').with_result(['a', 'a'])
