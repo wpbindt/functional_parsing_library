a70d5a26fb128822c388c78897f28eae0fb8b94f
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-02-17 22:16:18 +0100

    Add parsing of non-regular language

---
+++ parse_a_non_regular_language.py
@@ -0,0 +1,28 @@
+import unittest
+
+from asserts import assert_parsing_succeeds, assert_parsing_fails
+from char import char
+from recursive_parser import RecursiveParser
+
+a = char('a')
+b = char('b')
+wedged_term = RecursiveParser()
+non_regular_parser = (a & b) | wedged_term.parser
+wedged_term.parser = a & non_regular_parser & b
+
+
+class TestParseNonRegularLanguage(unittest.TestCase):
+    def test_basic_case(self) -> None:
+        assert_parsing_succeeds(self, non_regular_parser, 'ab')
+
+    def test_another_case(self) -> None:
+        assert_parsing_succeeds(self, non_regular_parser, 'aabb')
+
+    def test_parser_counts(self) -> None:
+        assert_parsing_succeeds(self, non_regular_parser, 'aabb')
+
+    def test_parser_fails_on_stuff_too(self) -> None:
+        assert_parsing_succeeds(self, non_regular_parser, 'aabbbb').with_remainder('bb')
+
+    def test_parser_fails_on_stuff_too_2(self) -> None:
+        assert_parsing_fails(self, non_regular_parser, 'aa')
