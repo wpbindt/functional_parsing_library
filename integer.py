85741cca287b2d61560684839e7460d71600d1ff
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-02-17 19:44:41 +0100

    Implement the dumbest thing ever

--- integer.py
+++ integer.py
@@ -4,11 +4,12 @@ import unittest
 from asserts import assert_parsing_succeeds
 from char import char
 from char_in import char_in
+from hacky_function_composition import o
 from many import many
 
 
 digit = char_in(string.digits)
-nonnegative_integer = int * (''.join * many(digit))
+nonnegative_integer = int /o/ ''.join * many(digit)
 negate = lambda x: -x
 negative_integer = negate * (char('-') > nonnegative_integer)
 integer = nonnegative_integer | negative_integer
