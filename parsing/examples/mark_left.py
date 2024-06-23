9087f1e987696c60e46816bb291e918205e1278f
wpbindt <wesselbindt@gmail.com>
2024-06-23 12:51:33 +0200

    Add regular text

--- parsing/examples/mark_left.py
+++ parsing/examples/mark_left.py
@@ -1,11 +1,27 @@
+import string
+from dataclasses import dataclass
+
 from asserts import assert_parsing_succeeds
-from parsing.combinators.many import some
+from parsing.combinators.many import some, many
+from parsing.strings.char_in import char_in
 
-MarkLeft = list
 
-mark_left_token = ...
+@dataclass(frozen=True)
+class RegularText:
+    content: str
+
+
+MarkLeft = list[RegularText]
+
+allowed_characters = ' ' + string.ascii_letters
+allowed_character = char_in(allowed_characters)
+mark_left_token = RegularText * (''.join * many(allowed_character))
 mark_left = some(mark_left_token)
 
 
 def test_that_nothing_is_parsed() -> None:
     assert_parsing_succeeds(mark_left, '').with_result([])
+
+
+def test_that_regular_text_gets_parsed_to_regular_text() -> None:
+    assert_parsing_succeeds(mark_left, 'hi there').with_result([RegularText('hi there')])
