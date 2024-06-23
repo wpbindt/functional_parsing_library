6fc4e19648e5dccd588e6b179318b501875b4cdb
wpbindt <wesselbindt@gmail.com>
2024-06-23 13:51:16 +0200

    Add newline to regular text

--- parsing/examples/mark_left.py
+++ parsing/examples/mark_left.py
@@ -22,8 +22,8 @@ MarkLeft = list[RegularText | NewLine]
 
 allowed_characters = ' ' + string.ascii_letters
 allowed_character = char_in(allowed_characters)
-new_line = const(NewLine()) * word('\n\n')
-regular_text = RegularText * (''.join * many(allowed_character))
+new_line = const(NewLine()) * word('\n')
+regular_text = (RegularText * (''.join * many(allowed_character))) < word('\n')
 mark_left_token = regular_text | new_line
 mark_left = some(mark_left_token)
 
@@ -33,14 +33,14 @@ def test_that_nothing_is_parsed() -> None:
 
 
 def test_that_regular_text_gets_parsed_to_regular_text() -> None:
-    assert_parsing_succeeds(mark_left, 'hi there').with_result([RegularText('hi there')])
+    assert_parsing_succeeds(mark_left, 'hi there\n').with_result([RegularText('hi there')])
 
 
 def test_that_new_lines_are_parsed_as_such() -> None:
-    input_ = 'hi there\n\nthis is another paragraph'
+    input_ = 'hi there\n\nthis is another paragraph\n'
     expected = [
         RegularText('hi there'),
         NewLine(),
         RegularText('this is another paragraph'),
     ]
-    assert_parsing_succeeds(mark_left, input_).with_result(expected)
+    assert_parsing_succeeds(mark_left, input_).with_result(expected).with_remainder('')
