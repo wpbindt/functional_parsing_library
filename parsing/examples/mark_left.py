ac597564f8b356331c5d92c546da3fd4beb25571
wpbindt <wesselbindt@gmail.com>
2024-06-24 12:04:14 +0200

    Parse mark left

--- parsing/examples/mark_left.py
+++ parsing/examples/mark_left.py
@@ -4,7 +4,7 @@ import pytest
 
 from asserts import assert_parsing_succeeds, assert_parsing_fails
 from parsing.combinators.lookahead import lookahead
-from parsing.combinators.many import many
+from parsing.combinators.many import many, some
 from parsing.examples.person import const
 from parsing.strings.char import char
 from parsing.strings.char_not_in import char_not_in
@@ -49,6 +49,9 @@ regular_text = RegularText * normal_words
 
 new_line = const(NewLine()) * lookahead(char('\n'), char('\n'))
 
+mark_left_token = regular_text | new_line
+mark_left = some(mark_left_token)
+
 
 @pytest.mark.parametrize('special_character', list(SPECIAL_CHARACTERS))
 def test_that_regular_text_skips_special_characters(special_character: str) -> None:
@@ -65,3 +68,16 @@ def test_that_new_line_does_not_parse_single_new_line() -> None:
 
 def test_that_new_line_parses_double_new_line_and_consumes_only_one() -> None:
     assert_parsing_succeeds(new_line, '\n\n').with_result(NewLine()).with_remainder('\n')
+
+
+def test_that_mark_left_parses_no_text() -> None:
+    assert_parsing_succeeds(mark_left, '').with_result([])
+
+
+def test_that_mark_left_parses_some_text() -> None:
+    to_parse = 'hi mom\n\n'
+    expected = [
+        RegularText('hi mom'),
+        NewLine(),
+    ]
+    assert_parsing_succeeds(mark_left, to_parse).with_result(expected)
