754ae994a7055dc4a0bbabd808812b80348f27be
wpbindt <wesselbindt@gmail.com>
2024-06-24 11:17:26 +0200

    Pass on failure message lookahead

--- parsing/combinators/lookahead.py
+++ parsing/combinators/lookahead.py
@@ -7,7 +7,7 @@ def lookahead(parser: Parser[S], look_for: Parser[U]) -> Parser[S]:
     def parser_(to_parse: str) -> ParseResults[S] | CouldNotParse:
         result_combined = (parser < look_for)(to_parse)
         if isinstance(result_combined, CouldNotParse):
-            return CouldNotParse()
+            return result_combined
         return parser(to_parse)
 
     return Parser(parser_)
@@ -21,8 +21,9 @@ def test_that_lookahead_looks_ahead() -> None:
 
 def test_that_lookahead_fails_when_lookahead_not_ahead() -> None:
     parser = lookahead(word('a'), look_for=word('b'))
+    expected_failure_message = word('b')('').reason
 
-    assert_parsing_fails(parser, 'a')
+    assert_parsing_fails(parser, 'a').with_reason(expected_failure_message)
 
 
 def test_that_lookahead_keeps_remainder() -> None:
