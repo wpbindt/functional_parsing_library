fbb1dbe6a743cd4181d5f1d0943cafe08d4e250f
wpbindt <wesselbindt@gmail.com>
2024-06-24 11:25:08 +0200

    Skip special characters

--- parsing/examples/mark_left.py
+++ parsing/examples/mark_left.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 
+import pytest
+
 from asserts import assert_parsing_succeeds
-from parsing.combinators.many_till_exclusive import many_till_exclusive
-from parsing.strings.any_char import any_char
-from parsing.strings.word import word
+from parsing.combinators.many import many
+from parsing.strings.char_not_in import char_not_in
 
 
 @dataclass(frozen=True)
@@ -37,11 +38,12 @@ class BoldText:
 MarkLeftToken = RegularText | NewLine | Header | BoldText
 
 
-SPECIAL_CHARACTERS = '#*\n'
+SPECIAL_CHARACTERS = '*\n'
 
 
-regular_text = many_till_exclusive(any_char, word('\n'))
+regular_text = many(char_not_in(SPECIAL_CHARACTERS))
 
 
-def test_that_regular_text_parses_until_new_line_exclusive() -> None:
-    assert_parsing_succeeds(regular_text, 'hi mom\n').with_remainder('\n')
+@pytest.mark.parametrize('special_character', list(SPECIAL_CHARACTERS))
+def test_that_regular_text_parses_until_new_line_exclusive(special_character: str) -> None:
+    assert_parsing_succeeds(regular_text, f'hi mom{special_character}').with_remainder(special_character)
