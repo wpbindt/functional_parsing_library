427c3f9ed2e22ef4de6e83bc78e38996c0c74b65
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-06-27 19:11:23 +0200

    Docstring for or

--- functional_parsing_library/combinators/parse_or.py
+++ functional_parsing_library/combinators/parse_or.py
@@ -2,6 +2,16 @@ from functional_parsing_library.parser import Parser, T, S, ParseResults, CouldN
 
 
 def or_2(parser_1: Parser[T], parser_2: Parser[S]) -> Parser[T | S]:
+    """
+    Combines two parsers into one which tries to match for the first one, and upon failure match for the second one.
+    The `|` operator is overloaded to call this function. For example,
+    >>> from functional_parsing_library.strings import char
+    >>> parser = char('a') | char('b')
+    >>> parser('a').result
+    'a'
+    >>> parser('b').result
+    'b'
+    """
     def parser(to_parse: str) -> ParseResults[T | S] | CouldNotParse:
         try_1 = parser_1(to_parse)
         if not isinstance(try_1, CouldNotParse):
