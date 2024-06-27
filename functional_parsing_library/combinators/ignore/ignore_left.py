fdf0c4e862b75be5c5226f1b7df7d7d133db850a
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-06-27 20:05:59 +0200

    Docstrings for ignore_left and right

--- functional_parsing_library/combinators/ignore/ignore_left.py
+++ functional_parsing_library/combinators/ignore/ignore_left.py
@@ -2,4 +2,12 @@ from functional_parsing_library.parser import Parser, S, T
 
 
 def ignore_left(left: Parser[T], right: Parser[S]) -> Parser[S]:
+    """
+    Parses using the left parser, discards the result, and then parses using the right. The `>` operator is overloaded
+    to call this function. For example,
+    >>> from functional_parsing_library.strings import char
+    >>> parser = char('a') > char('b')
+    >>> parser('ab').result
+    'b'
+    """
     return (lambda t, s: s) * left & right
--- functional_parsing_library/combinators/ignore/ignore_right.py
+++ functional_parsing_library/combinators/ignore/ignore_right.py
@@ -2,4 +2,16 @@ from functional_parsing_library.parser import Parser, S, T
 
 
 def ignore_right(left: Parser[T], right: Parser[S]) -> Parser[T]:
+    """
+    Parses using the left parser, and then parses using the right. The result from the left parser is discarded. If the
+    right parser is unable to match something, the parser fails. The `<` operator is overloaded to call this function.
+    For example,
+    >>> from functional_parsing_library.strings import char
+    >>> parser = char('a') < char('b')
+    >>> parser('ab').result
+    'a'
+    >>> from functional_parsing_library.parser import CouldNotParse
+    >>> isinstance(parser('a'), CouldNotParse)
+    True
+    """
     return (lambda t, s: t) * left & right
