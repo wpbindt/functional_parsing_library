be8c0cfed0f43ebe1728a0ecd698bce2181f3b2e
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-06-28 17:38:25 +0200

    Explicit any

--- functional_parsing_library/strings/modules/char_does_not_match.py
+++ functional_parsing_library/strings/modules/char_does_not_match.py
@@ -1,9 +1,11 @@
+from typing import Any
+
 from functional_parsing_library.check_for_empty_string import check_for_empty_string
-from functional_parsing_library.parser import U, Parser, ParseResults, CouldNotParse
+from functional_parsing_library.parser import Parser, ParseResults, CouldNotParse
 from functional_parsing_library.strings.modules.pop_one_character import pop_one_character
 
 
-def char_does_not_match(parser: Parser[U]) -> Parser[str]:
+def char_does_not_match(parser: Parser[Any]) -> Parser[str]:
     """
     Match any character not matching the given parser. For example,
     >>> from functional_parsing_library.strings import digit
@@ -22,5 +24,3 @@ def char_does_not_match(parser: Parser[U]) -> Parser[str]:
         return pop_one_character(to_parse)
 
     return Parser(parser_)
-
-
