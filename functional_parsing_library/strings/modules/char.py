394b69788075307ebf82b5ae1be3dbec1e9bc6e2
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-06-29 10:54:49 +0200

    Refactor char to make use of char_satisfies

--- functional_parsing_library/strings/modules/char.py
+++ functional_parsing_library/strings/modules/char.py
@@ -1,6 +1,5 @@
-from functional_parsing_library.check_for_empty_string import check_for_empty_string
-from functional_parsing_library.parser import Parser, ParseResults, CouldNotParse
-from functional_parsing_library.strings.modules.pop_one_character import pop_one_character
+from functional_parsing_library.parser import Parser
+from functional_parsing_library.strings.modules.char_satisfies import char_satisfies
 
 
 def char(c: str) -> Parser[str]:
@@ -14,11 +13,7 @@ def char(c: str) -> Parser[str]:
     >>> isinstance(char('a')('bla'), CouldNotParse)
     True
     """
-    @check_for_empty_string
-    def parser(to_parse: str) -> ParseResults[str] | CouldNotParse:
-        if to_parse[0] != c:
-            return CouldNotParse(f'String "{to_parse}" does not start with "{c}"')
-
-        return pop_one_character(to_parse)
-
-    return Parser(parser)
+    return char_satisfies(
+        condition=lambda x: x == c,
+        reason_factory=lambda to_parse: f'String "{to_parse}" does not start with "{c}"',
+    )
--- functional_parsing_library/strings/modules/char_satisfies.py
+++ functional_parsing_library/strings/modules/char_satisfies.py
@@ -1,11 +1,14 @@
 from typing import Callable
 
 from functional_parsing_library.check_for_empty_string import check_for_empty_string
-from functional_parsing_library.parser import Parser, ParseResults, CouldNotParse
+from functional_parsing_library.parser import Parser, ParseResults, CouldNotParse, FailureReason, NoReasonSpecified
 from functional_parsing_library.strings.modules.pop_one_character import pop_one_character
 
 
-def char_satisfies(condition: Callable[[str], bool]) -> Parser[str]:
+def char_satisfies(
+    condition: Callable[[str], bool],
+    reason_factory: Callable[[str], FailureReason] = lambda to_parse: NoReasonSpecified(),
+) -> Parser[str]:
     """
     Match any character satisfying the given condition. For example,
     >>> char_satisfies(lambda c: c == 'b')('b').result
@@ -15,5 +18,5 @@ def char_satisfies(condition: Callable[[str], bool]) -> Parser[str]:
     def parser(to_parse: str) -> ParseResults[str] | CouldNotParse:
         if condition(to_parse[0]):
             return pop_one_character(to_parse)
-        return CouldNotParse()
+        return CouldNotParse(reason_factory(to_parse))
     return Parser(parser)
