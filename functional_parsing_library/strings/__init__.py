5c2e0ea37cb663d4be5332f1dc7bca26f4eaf313
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-06-30 13:13:27 +0200

    Put char_satisfies in global namespace

--- functional_parsing_library/strings/__init__.py
+++ functional_parsing_library/strings/__init__.py
@@ -1,4 +1,5 @@
 from functional_parsing_library.strings.modules.char import char
+from functional_parsing_library.strings.modules.char_satisfies import char_satisfies
 from functional_parsing_library.strings.modules.word import word
 from functional_parsing_library.strings.modules.char_in import char_in
 from functional_parsing_library.strings.modules.any_char import any_char
@@ -7,11 +8,12 @@ from functional_parsing_library.strings.modules.char_not_in import char_not_in
 from functional_parsing_library.strings.modules.digit import digit
 
 __all__ = [
-    'char',
-    'word',
-    'char_in',
     'any_char',
-    'char_not_in',
+    'char',
     'char_does_not_match',
+    'char_in',
+    'char_not_in',
+    'char_satisfies',
     'digit',
+    'word',
 ]
