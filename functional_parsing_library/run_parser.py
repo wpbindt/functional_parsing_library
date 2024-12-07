788e59054314ea624afc23140cc1b41cf6b771a4
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-12-07 15:36:38 +0100

    Raise on remainder

--- functional_parsing_library/run_parser.py
+++ functional_parsing_library/run_parser.py
@@ -5,8 +5,21 @@ class ParsingError(Exception):
     pass
 
 
-def run_parser(parser: Parser[S], to_parse: str) -> S:
+class NotFullyParsed(Exception):
+    pass
+
+
+def run_parser(
+    parser: Parser[S],
+    to_parse: str,
+    raise_on_remainder: bool = False,
+) -> S:
     parse_result = parser(to_parse)
+
     if isinstance(parse_result, CouldNotParse):
         raise ParsingError
+
+    if raise_on_remainder and len(parse_result.remainder) > 0:
+        raise NotFullyParsed
+
     return parse_result.result
